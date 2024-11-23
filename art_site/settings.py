b4a7c933532a49f008a1ba984d0f80d28613f61d
dazeez1 <d.azeez@alustudent.com>
2024-11-24 01:10:41 +0200

    reconfigured

--- art_site/settings.py
+++ art_site/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'AfriCreate/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'AfriCreate/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' 'AfriCreate/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' '/AfriCreate/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
